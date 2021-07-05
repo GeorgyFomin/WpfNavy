3e3b9cb3d8bb68002a46eab42853a1ac788e2422
Georgy <ajvbyud@outlook.com>
2021-07-05 08:48:54 +0300

    Code behind handlers edit name adjusting

--- View/MainWindow.xaml.cs
+++ View/MainWindow.xaml.cs
@@ -1,5 +1,7 @@
 ﻿using System.ComponentModel;
 using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Input;
 
 namespace WpfNavy
 {
@@ -12,6 +14,16 @@ namespace WpfNavy
         public MainWindow()
         {
             InitializeComponent();
+        }
+        private void AdjustColumnWidth(GridViewColumn column)
+        {
+            for (int i = 0; i < 2; i++)
+                column.Width = double.IsNaN(column.Width) ? column.ActualWidth : double.NaN;
+        }
+        private void TextBoxEditNameHandler(object sender, RoutedEventArgs e)
+        {
+            if ((e is KeyEventArgs arg && arg.Key == Key.Enter) || !(e is KeyEventArgs))
+                AdjustColumnWidth((sender as TextBox).Name == "clientNameBox" ? clientNameСolumn : depNameColumn);
         }
     }
 }
